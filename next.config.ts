f786215aab752ba3a18bd204b3df1e90a17dd966
Lucasf-Ramos <drawingworldab@gmail.com>
2025-12-17 10:00:02 -0300

    contact form fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "cdn.exemplo.com",
+      "images.unsplash.com",
+      "i.imgur.com",
+      "media.discordapp.net"
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
